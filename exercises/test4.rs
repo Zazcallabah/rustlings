7e5b593582f005ec2bbd5a356891336ad88f2154
Peter Hamberg <zazcallabah@gmail.com>
2019-10-24 11:51:03 +0200

    add macros and moves

--- exercises/test4.rs
+++ exercises/test4.rs
@@ -5,6 +5,16 @@
 
 // Write a macro that passes the test! No hints this time, you can do it!
 
+#[macro_use]
+mod macros {
+    macro_rules! my_macro {
+        ($val:expr) => {
+            "Hello ".to_owned()+$val
+        }
+    }
+
+}
+
 fn main() {
     if my_macro!("world!") != "Hello world!" {
         panic!("Oh no! Wrong output!");
