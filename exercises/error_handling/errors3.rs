ba46478b1d4fce5f18229fd8f17d26ae5f6d72fa
Peter Hamberg <zazcallabah@gmail.com>
2019-10-24 15:24:10 +0200

    add errors

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -10,7 +10,11 @@ fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
+    let cost = total_cost(pretend_user_input);
+    let cost = match cost {
+        Ok(cost) => cost,
+        Err(msg) => panic!("Bad input"),
+    };
 
     if cost > tokens {
         println!("You can't afford that many!");
